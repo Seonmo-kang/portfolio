271cf6e4d6bf5b4465a8a34bf4c606f0fdc1f9d9
Seonmo <tjsah18@gmail.com>
2022-07-12 21:11:05 -0400

    Feat : add about section

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Banner from './componenets/banner/Banner';
 import NavBar from './componenets/navbar/NavBar';
+import About from './componenets/about/About';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Navbar_test from './componenets/navbar/Navbar_test';
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Banner></Banner>
+      <About></About>
     </>
   );
 }
---
+++ portfolio-app/src/componenets/about/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const About = () => {
+  return (
+    <section className='about' id="about">
+        <Container>
+            <Row>
+                <Col className='p-md-3 p-0'>
+                    
+                </Col>
+            </Row>
+        </Container>
+    </section>
+  )
+}
+
+export default About
