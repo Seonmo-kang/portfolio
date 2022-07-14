8f35b18df598ac21e14f6baec0aa8cb1e6a896cd
Seonmo <tjsah18@gmail.com>
2022-07-14 17:20:37 -0400

    Feat : Add About section

--- portfolio-app/src/componenets/about/About.js
+++ portfolio-app/src/componenets/about/About.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import './about.scss';
 
 const About = () => {
   return (
     <section className='about' id="about">
         <Container>
-            <Row>
+            <Row className="about-section">
                 <Col className='p-md-3 p-0'>
-                    
+                    <div className='about-bx p-md-3'>
+                      About me
+                    </div>
+                    <div className='description'>
+                      <p>Hello, I am Seonmo! </p>
+                    </div>
                 </Col>
             </Row>
         </Container>
